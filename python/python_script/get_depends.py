7f464357740e67b5660d2a8e7b74267e3d6cf933
gaoyuxia <gaoyuxia@wind-mobi.com>
2019-06-12 14:47:43 +0800

    add in 201906121447

---
+++ python/python_script/get_depends.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import sys
+import json
+
+sys.setrecursionlimit(1500)
+dd={}
+topList=[]
+
+with open('data.json') as jf:
+	for line in jf.readlines():
+		dd=json.loads(line)
+		print dd
+		
+
+def getValue(key,topList):
+	if key not in topList:
+		topList.append(key)
+		result=dd.get(str(key),"error")
+		if result == "error":
+			print "error"
+			pass
+		else:
+			if len(result) == 0:
+				return
+			else:
+				for subkey in result:
+					getValue(subkey,topList)
+			return topList
+
+for key in dd.keys():
+	getValue(key,topList)
+	print topList
+	
